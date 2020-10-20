ddb5b2f5af7ea57134096a29d76ba4667e81407e
Mukesh Ravichandran <69010229+mukesh-ravichandran@users.noreply.github.com>
2020-10-19 21:49:20 -0400

    Final model with atlear one model

--- TS_Final_Project.R
+++ TS_Final_Project.R
@@ -16,7 +16,9 @@ library(data.table)
 library(imputeTS)
 library(gridExtra)
 
-setwd("/Users/mukeshravichandran/OneDrive - North Carolina State University/MSA-Mukesh’s MacBook Pro/Courses/Fall/AA502/Fall 1/Time Series/Project/Data")
+setwd("/Users/mukeshravichandran/OneDrive - North Carolina State University/MSA-Mukesh’s MacBook Pro/Courses/Fall/AA502/Fall 1/Time Series/Project/Code/Ozone_Prediction/Data")
+
+#Theme for graphs
 my_theme = theme_minimal()+theme(plot.title = element_text(hjust = 0.5), 
                       panel.background = element_rect(fill='white')) + theme(panel.grid.major = element_blank(),
                                                                           panel.grid.minor = element_blank())
@@ -42,7 +44,7 @@ white.noise = function(df) {
   return(White.LB)
 }
 
-#adftest
+#adftest for checking stationarity
 
 adf= function(series) {
 
@@ -77,7 +79,7 @@ training= subset(ozone_ts,end=length(ozone_ts)-(28+14))
 validation= subset(ozone_ts,start=length(ozone_ts)-(28+14-1), end=length(ozone_ts)-14)
 testing= subset(ozone_ts,start=length(ozone_ts)-(13), end=length(ozone_ts))
 
-autoplot(training)
+autoplot(training)+my_theme
 
 #checking if the split is done properly
 length(training)
@@ -88,9 +90,13 @@ sum(ozone_ts == c(training,validation,testing)) == length(ozone_ts)
 
 #####ARIMA Modelling#####
 
+#creating regression variables
+# one for easonaluty and one for trend
+xreg1<-cbind(fourier(training,K=6),seq(1,length(training)))
+colnames(xreg1)<-c('s1','c1','s2','c2','s3','c3','s4','c4','s5','c5','s6','c6','time')
 
 
-Trial_1_Seasonal<-Arima(training,order=c(0,0,0),seasonal=c(0,0,0),xreg=fourier(training,K=6))
+Trial_1_Seasonal<-Arima(training,order=c(0,0,0),seasonal=c(0,0,0),xreg=xreg1)
 
 
 autoplot(Trial_1_Seasonal$residuals,color="red")+ geom_hline(yintercept=0,linetype="dashed")
@@ -112,18 +118,28 @@ auto.arima(Trial_1_Seasonal$residuals)
 
 #it says an AR2 model, lets check that
 
-Trial_2_Seasonal<-Arima(training,order=c(2,0,0),seasonal=c(0,0,0),xreg=fourier(training,K=6))
+Trial_2_Seasonal<-Arima(training,order=c(3,0,0),seasonal=c(0,0,0),xreg=xreg1)
 autoplot(Trial_2_Seasonal$residuals,color="red")+ geom_hline(yintercept=0,linetype="dashed") #definitely better than the first one
 
 adf(Trial_2_Seasonal$residuals)
 
 white.noise(Trial_2_Seasonal$residuals) # yaay white noise
 summary(Trial_2_Seasonal)
-autoplot(forecast(Trial_2_Seasonal,xreg = fourier(training,K=6)[109.5:509.25,])) + my_theme + labs(title = "ARIMA Model", y= "Daily 8 Hour Maximum Ozone Concentration", X= "Date")
+
+
+
+autoplot(forecast(Trial_2_Seasonal,xreg = xreg1[109.5:137.5,])) + my_theme + labs(title = "ARIMA Model", y= "Daily 8 Hour Maximum Ozone Concentration", X= "Date")
 
 #Mape = 17.74585
 #MAE = 0.005807725
 #on the training data
+
+arima.forecast= forecast(Trial_2_Seasonal,xreg = xreg1[109.5:137.5,])[["mean"]]
+
+
+MAE(arima.forecast,validation) 
+#MAPE and MAE of ARIMAX model on the validation == 11.7 and 0.0045
+
 
 ##### ARIMAX Modelling #####
 
@@ -220,8 +236,15 @@ grid.arrange(plot1+
 
 
 ####Arima to model NO2####
+training_NO2= subset(NO2_ts,end=length(NO2_ts)-(28+14))
+validation_NO2= subset(NO2_ts,start=length(NO2_ts)-(28+14-1), end=length(NO2_ts)-14)
+testing_NO2= subset(ozone_ts,start=length(NO2_ts)-(13), end=length(NO2_ts))
 
-NO2_Seasonal<-Arima(NO2_ts,order=c(0,0,0),seasonal=c(0,0,0),xreg=fourier(NO2_ts,K=6))
+xreg1_NO2<-cbind(fourier(training_NO2,K=6),seq(1,length(training_NO2)))
+colnames(xreg1_NO2)<-c('s1','c1','s2','c2','s3','c3','s4','c4','s5','c5','s6','c6','time')
+
+
+NO2_Seasonal<-Arima(training_NO2,order=c(0,0,0),seasonal=c(0,0,0),xreg=xreg1_NO2)
 
 autoplot(NO2_Seasonal$residuals)
 
@@ -234,7 +257,7 @@ Pacf(NO2_Seasonal$residuals,lag.max = 400)$acf
 
 auto.arima(NO2_Seasonal$residuals)
 
-NO2_Seasonal_2<-Arima(NO2_ts,order=c(5,0,0),seasonal=c(0,0,0),xreg=fourier(NO2_ts,K=6))
+NO2_Seasonal_2<-Arima(training_NO2,order=c(2,0,0),seasonal=c(0,0,0),xreg=xreg1_NO2)
 
 adf(NO2_Seasonal_2$residuals)
 white.noise(NO2_Seasonal_2$residuals)    #yaay white noise
@@ -248,7 +271,20 @@ ccf(NO2_Seasonal_2$residuals,Trial_2_Seasonal$residuals,lag.max = 10)
 
 
 ####Arima to model TMAX####
-tmax_seasonal<-Arima(tmax_ts,order=c(0,0,0),seasonal=c(0,0,0),xreg=fourier(tmax_ts,K=6))
+
+training_tmax= subset(tmax_ts,end=2301)
+validation_tmax= subset(tmax_ts,start=2302, end=2329)
+testing_tmax= subset(tmax_ts,start=2330, end=2343)
+
+xreg1_tmax<-cbind(fourier(training_tmax,K=6),seq(1,length(training_tmax)))
+colnames(xreg1_tmax)<-c('s1','c1','s2','c2','s3','c3','s4','c4','s5','c5','s6','c6','time')
+
+
+
+
+
+
+tmax_seasonal<-Arima(training_tmax,order=c(0,0,0),seasonal=c(0,0,0),xreg=xreg1_tmax)
 
 autoplot(tmax_seasonal$residuals)
 
@@ -261,10 +297,10 @@ Pacf(tmax_seasonal$residuals,lag.max = 400)$acf
 
 auto.arima(tmax_seasonal$residuals)
 
-tmax_seasonal_2<-Arima(tmax_ts,order=c(5,0,0),seasonal=c(0,0,0),xreg=fourier(tmax_ts,K=6))
+tmax_seasonal_2<-Arima(training_tmax,order=c(3,0,0),seasonal=c(0,0,0),xreg=xreg1_tmax)
 
-adf(NO2_Seasonal_2$residuals)
-white.noise(NO2_Seasonal_2$residuals)    #yaay white noise
+adf(tmax_seasonal_2$residuals)
+white.noise(tmax_seasonal_2$residuals)    #yaay white noise
 
 
 
@@ -275,49 +311,74 @@ ccf(tmax_seasonal_2$residuals,Trial_2_Seasonal$residuals,lag.max = 10)
 
 
 
-####Arima to model CO####
 
 
-CO_seasonal<-Arima(CO_ts,order=c(0,0,0),seasonal=c(0,0,0),xreg=fourier(CO_ts,K=6))
-
-autoplot(CO_seasonal$residuals)
-
-adf(CO_seasonal$residuals)
-white.noise(CO_seasonal$residuals)
-
-Acf(CO_seasonal$residuals)$acf
-Pacf(CO_seasonal$residuals,lag.max = 400)$acf
-
-
-auto.arima(CO_seasonal$residuals)
-
-CO_seasonal_2<-Arima(CO_ts,order=c(5,0,0),seasonal=c(0,0,0),xreg=fourier(CO_ts,K=6))
-
-adf(CO_seasonal_2$residuals)
-white.noise(CO_seasonal_2$residuals)    #yaay white noise
-
-ccf(CO_seasonal_2$residuals,Trial_2_Seasonal$residuals,lag.max = 10) 
-
-#again lag at 0,1 and 2 ... which is not a good thing lol!
-
-arimax= Arima(ozone_ts,order=c(0,0,0),seasonal=c(0,0,0),xreg=cbind(fourier(ozone_ts,K=6),tmax_seasonal_2$residuals,NO2_Seasonal_2$residuals))
+arimax= Arima(training,order=c(0,0,0),seasonal=c(0,0,0),xreg=cbind(xreg1,training_NO2,training_tmax))
 
 adf(arimax$residuals)
 white.noise(arimax$residuals)
 
 auto.arima(arimax$residuals)
 
-arimax_2= Arima(ozone_ts,order=c(1,0,0),seasonal=c(0,0,0),xreg=cbind(fourier(ozone_ts,K=6),tmax_seasonal_2$residuals))
+arimax_2= Arima(training,order=c(1,0,0),seasonal=c(0,0,0),xreg=cbind(xreg1,training_NO2,training_tmax))
 
 summary(arimax_2)
 
+#MAPE 0.005375724
+#MAE 16.52266
 
-#MAPE 17.08 --? NOT A GREAT IMPROVEMENT AFTER ADDING TWO MORE VARIABLES. MIGHT IMPACT ON VALIDATION
+arimax.forecast= forecast(arimax_2,xreg = cbind(xreg1[109.5:136.5,],validation_NO2,validation_tmax))[["mean"]]
 
+
+MAPE(arimax.forecast,validation) 
+
+
+#MAPE 0.086
+#MAE 0.00348
 
 #####NEURAL NETWORKS#####
 
+NN.Model <- nnetar(training, p = 3, size = 2)
+NN.Forecast <- forecast(NN.Model, h = 200)
+plot(NN.Forecast)
 
 
+Model.four<-Arima(training,order=c(0,0,0),xreg=xreg1)
+NN.Model2<-nnetar(Model.four$residuals,p=3,size=2)
+NN.Forecast2<-forecast(NN.Model2,h=500)
+plot(NN.Forecast2)
+
+Pass.Forecast <- rep(NA, 28)
+for(i in 1:365){
+  Pass.Forecast[i] <- training[length(training) - 365 + i] + forecast(NN.Model2, h = 28)$mean[i]
+}
+
+Pass.Forecast <- ts(Pass.Forecast, start = 2020.29979466119, frequency = 365.24)
+
+plot(training, xlim = c(2014, 2021))
+lines(Pass.Forecast, col = "blue")
+abline(v = 2020.29979466119, col = "red", lty = "dashed")
+
+NN.forecast= Pass.Forecast[1:28]
+
+MAE(NN.forecast,validation)
+
+#MAPe = 23.12412
+#MAe = 0.009
+
+##Can definitely do better with NN - something is off
+
+
+#ENSEMBLE MODELLING
+
+
+  
+Ensemble1= (arima.forecast + arimax.forecast + NN.forecast)/3
+
+MAPE(Ensemble1, validation)
+
+Ensemble2= (arima.forecast + arimax.forecast)/2
+  
+MAPE(Ensemble2, validation)
 
 
